c6088661a66d286f46f8994ceb4acb120ae7617f
thombot <thombot43@gmail.com>
2024-05-09 21:32:10 +0100

    fixing

--- flash_app.py
+++ flash_app.py
@@ -19,7 +19,6 @@ def create_card():
         quit = input("Add another? (y/n) ")
         if quit == "n":
             return False
-        print(list_cards())
 
 def list_cards():
     # for card in cards:
@@ -32,46 +31,37 @@ def list_all_cards():
         print(f"Total number of cards: {len(cards)}")
         print("--------------------------")
         for card in cards:
-            print(f"> {magenta_color} {card['sl']} {reset_text}\t=\t{orange_color} {card['tl']} {reset_text} ")
+            print(f"{magenta_color} {card['sl']} {reset_text}\t=\t{orange_color} {card['tl']} {reset_text} ")
 
 def quiz():
-    # get a random subset of all cards (10 maybe)
-    # present the english version of the word and learner guesses the dutch
-    # keep a score
+    '''
+    get a random subset of all cards (10 maybe)
+    present the english version of the word and learner guesses the dutch
+    keep a score
+    ''' 
     if not cards or len(cards) < 3:
         print("Sorry! There aren't enough cards to test. Try creating some more.")
     else:
-        review_session = random.sample(cards, 3)
+        quiz_session = random.sample(cards, 3)
 
         count = 0
         score = 0
 
-        print(f"# You will be tested on {len(review_session)} words")
+        print(f"# You will be tested on {len(quiz_session)} words")
 
-        while count < len(review_session):
-            print(f"# Score: {score}/{len(review_session)}")
-            word = random.choice(review_session) # bug
-            print(orange_color + word['sl'] + reset_text)
-            to_guess = word['tl']
-            guess = input("> ")
-            if guess == to_guess:
-                print(green_color + "correct!" + reset_text)
-                score += 1; 
-            else:
-                print(red_color + "Incorrect!" + reset_text)
-            count += 1
-        print(bold_text + f"You scored {score}/{len(review_session)} and got {round(score / len(review_session) * 100)}%" + reset_text)
-
-def random_card():
-    pass
-    # r = random.choice(cards)
-    # print(r)
-
-def check_guess():
-    # if the key
-    pass
-
-# random_card()
+        while count < len(quiz_session):
+            for word in quiz_session:
+                print(f"# Score: {score}/{len(quiz_session)}")
+                print(orange_color + word['sl'] + reset_text)
+                to_guess = word['tl']
+                guess = input("> ")
+                if guess == to_guess:
+                    print(green_color + "correct!" + reset_text)
+                    score += 1; 
+                else:
+                    print(red_color + "Incorrect!" + reset_text)
+                count += 1
+        print(bold_text + f"You scored {score}/{len(quiz_session)} and got {round(score / len(quiz_session) * 100)}%" + reset_text)
 
 def review(cards):
     '''
@@ -81,7 +71,7 @@ def review(cards):
         print("Sorry! There are no cards to review. Try creating some first.")
     else:
         print("Let's Review!")
-        # review_session = random.shuffle(words, 1)
+        # quiz_session = random.shuffle(words, 1)
         
         count = 0
         while count < len(cards):
@@ -126,7 +116,7 @@ r'''
 print(bold_text + "\033[1m" "Welcome to Flash! What would you like to do? \n" + reset_text )
 
 while True:
-    choice = input(yellow_color + "(0) Review / (1) Test / (2) Add cards / (3) Stats / (4) List cards / (5) Exit"  + reset_text + "\n>>> " )
+    choice = input(yellow_color + "(0) Review / (1) Quiz / (2) Add cards / (3) Stats / (4) List cards / (5) Exit"  + reset_text + "\n>>> " )
     if choice == "0":
         review(cards)
     elif choice == "1":
@@ -134,11 +124,11 @@ while True:
     elif choice == "2":
         create_card()
     elif choice == "3":
+        list_all_cards()
+    elif choice == "4":
         print("Stats:")
         print(f"Cards: 55")
-    elif choice == "4":
-        list_all_cards()
-    elif choice == "5":
+    elif choice in ["5", "q", "quit"]:
         print("Goodbye! / Tot ziens!")
         sys.exit()
     else:
